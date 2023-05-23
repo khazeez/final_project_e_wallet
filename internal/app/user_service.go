fa87bd8d407e428d6547ca8330369fb81f4f60ba
Made_Widhi_Widana <widhiwidana21@gmail.com>
2023-05-24 01:08:36 +0700

    [feature] repository dan handler user

--- internal/app/user_service.go
+++ internal/app/user_service.go
@@ -1,1 +1,62 @@
 package app
+
+import (
+	"github.com/KhoirulAziz99/final_project_e_wallet/internal/domain"
+	"github.com/KhoirulAziz99/final_project_e_wallet/internal/repository"
+	"errors"
+)
+type UserUsecase interface {
+	InsertUser(*domain.User) error
+	UpdateUser(*domain.User) error
+	FindOne(id int) (*domain.User, error)
+	FindAll() ([]domain.User, error)
+	Delete(id int) error
+}
+type userUsecase struct {
+	userRepository repository.UserRepository
+}
+
+func NewUserUsecase(userRepository repository.UserRepository) UserUsecase {
+	return &userUsecase{
+		userRepository: userRepository,
+	}
+}
+
+func (u *userUsecase) InsertUser(user *domain.User) error {
+	
+	if user.Name == "" {
+		return errors.New("name is required")
+	}
+	if user.Email == "" {
+		return errors.New("email is required")
+	}
+	if user.Password == "" {
+		return errors.New("password is required")
+	}
+	return u.userRepository.Create(user)
+}
+
+func (u *userUsecase) UpdateUser(user *domain.User) error {
+	if user.Name == "" {
+		return errors.New("name is required")
+	}
+	if user.Email == "" {
+		return errors.New("email is required")
+	}
+	if user.Password == "" {
+		return errors.New("password is required")
+	}
+	return u.userRepository.Update(user)
+}
+
+func (u *userUsecase) FindOne(id int) (*domain.User, error) {
+	return u.userRepository.FindOne(id)
+}
+
+func (u *userUsecase) FindAll() ([]domain.User, error) {
+	return u.userRepository.FindAll()
+}
+
+func (u *userUsecase) Delete(id int) error {
+	return u.userRepository.Delete(id)
+}
